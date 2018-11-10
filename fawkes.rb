f2693c73ee9655912eeb2ed69cb643774295d727
Josh Dunn <joshdunn.uwo@gmail.com>
2018-11-09 21:03:41 -0500

    better naming

---
+++ fawkes.rb
@@ -0,0 +1,11 @@
+class Fawkes < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/joshddunn/fawkes/archive/v0.0.0.tar.gz"
+  sha256 "2f794f7b13401d49915ddea487e77cc412fdf9806b644883b4d04b8328354bf3"
+
+  def install
+    bin.install 'fawkes'
+  end
+
+end

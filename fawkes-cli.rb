b07c6144e95cee91bfce28e9ee1f8bd9f27f9551
Josh Dunn <joshdunn.uwo@gmail.com>
2018-11-09 20:58:57 -0500

    first commit

---
+++ fawkes-cli.rb
@@ -0,0 +1,11 @@
+class FawkesCli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/joshddunn/fawkes-cli/archive/v0.0.0.tar.gz"
+  sha256 "b66f75704b93111d2f12a270bfc1b75a365ce7a2fda59630183ecfb752520b2b"
+
+  def install
+    bin.install 'fawkes'
+  end
+
+end
